d84d049856834b64015583bbf4f34808574c6f14
Stars0628 <43928335+stars0628@users.noreply.github.com>
2023-05-08 14:48:22 +0800

    Update soft_iic.c

--- 软件IIC/soft_iic.c
+++ 软件IIC/soft_iic.c
@@ -19,19 +19,36 @@ static void analog_i2c_delay(void)//出现异常或延时较大需适当增减�
  * @author Mr.W
  * @date 2020-10-12
  */
-void bsp_analog_i2c_init(void)//未在CubeMX配置才需要用
+void bsp_analog_i2c_init(void)
 {
-//    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    /* ------配置SDA和SCL为输出模式begin------------ */
 
-//    __HAL_RCC_GPIOC_CLK_ENABLE();
 
-//    /*Configure GPIO pins*/
-//    GPIO_InitStruct.Pin = sda_pin|scl_pin;
-//    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-//    GPIO_InitStruct.Pull = GPIO_NOPULL;
-//    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-//    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-//    bsp_analog_i2c_stop();
+
+    /* ------配置SDA和SCL为输出模式end------------ */
+    bsp_analog_i2c_stop();//配置完SDA和SCL后将引脚设为高电平，即IIC空闲状态
+}
+
+/**
+ * @brief SDA输出配置
+ * @retval none
+ * @author Mr.W
+ * @date 2020-10-12
+ */
+void I2Cx_SDA_OUT(void)
+{
+    //配置SDA为输出
+}
+
+/**
+ * @brief SDA输入配置
+ * @retval none
+ * @author Mr.W
+ * @date 2020-10-12
+ */
+void I2Cx_SDA_IN(void)
+{
+    //配置SDA为输入
 }
 
 /**
@@ -47,6 +64,7 @@ void bsp_analog_i2c_start(void)
      *    __________
      *SCL           \________
      */
+    I2Cx_SDA_OUT();
     i2c_sda_high();
     i2c_scl_high();
     analog_i2c_delay();
@@ -69,6 +87,7 @@ void bsp_analog_i2c_stop(void)
      *          ____________
      *SCL _____/
      */
+    I2Cx_SDA_OUT();
     i2c_sda_low();
     i2c_scl_high();
     analog_i2c_delay();
@@ -85,7 +104,7 @@ void bsp_analog_i2c_stop(void)
 uint8_t bsp_analog_i2c_wait_ack(void)
 {
     uint32_t timeout = 0;
-
+    I2Cx_SDA_IN();
     i2c_sda_high();
     analog_i2c_delay();
     i2c_scl_high();
@@ -95,12 +114,13 @@ uint8_t bsp_analog_i2c_wait_ack(void)
         timeout++;
         if(timeout > 2000)
         {
-            return 0;
+            bsp_analog_i2c_stop();
+            return 0;//返回0无应答
         }
     }
     i2c_scl_low();
     analog_i2c_delay();
-    return 1;
+    return 1;//返回1有应答
 }
 
 /**
@@ -116,6 +136,9 @@ void bsp_analog_i2c_ack(void)
      *    ____         _____
      *SDA     \_______/
      */
+    I2Cx_SDA_OUT();
+    i2c_scl_low();
+    analog_i2c_delay();
     i2c_sda_low();
     analog_i2c_delay();
     i2c_scl_high();
@@ -123,6 +146,7 @@ void bsp_analog_i2c_ack(void)
     i2c_scl_low();
     analog_i2c_delay();
     i2c_sda_high();
+    analog_i2c_delay();
 }
 
 /**
@@ -138,6 +162,9 @@ void bsp_analog_i2c_nack(void)
      *    __________________
      *SDA
      */
+    I2Cx_SDA_OUT();
+    i2c_scl_low();
+    analog_i2c_delay();
     i2c_sda_high();
     analog_i2c_delay();
     i2c_scl_high();
@@ -155,12 +182,14 @@ void bsp_analog_i2c_nack(void)
 void bsp_analog_i2c_send_byte(uint8_t data)
 {
     uint8_t i;
-
+    I2Cx_SDA_OUT();
+    i2c_scl_low();
+    analog_i2c_delay();
     for(i = 0; i < 8; i++)
     {
-        if(data & 0x80)
+        if(data & 0x80)//判断高位是不是1
         {
-           i2c_sda_high();
+            i2c_sda_high();
         }
         else
         {
@@ -173,10 +202,9 @@ void bsp_analog_i2c_send_byte(uint8_t data)
         i2c_scl_low();
         if(i == 7)
         {
-            i2c_sda_high();
+            i2c_sda_high();//发送最后一位之后，释放SDA总线，方便接收来自接收端的应答信号
         }
         data <<= 1;
-        analog_i2c_delay();
     }
 }
 
@@ -186,22 +214,27 @@ void bsp_analog_i2c_send_byte(uint8_t data)
  * @author Mr.W
  * @date 2020-10-12
  */
-uint8_t bsp_analog_i2c_read_byte(void)
+//读1个字节，ack=1时，发送ACK，ack=0，发送nACK
+uint8_t bsp_analog_i2c_read_byte(uint8_t ack)
 {
     uint8_t i, data = 0;
-
+    I2Cx_SDA_IN();//SDA设置为输入
     for(i = 0; i < 8; i++ )
     {
-        data <<= 1;
+        i2c_scl_low();
+        analog_i2c_delay();
         i2c_scl_high();
+        data <<= 1;
         analog_i2c_delay();
         if(i2c_read_sda())
         {
             data++;
         }
-        i2c_scl_low();
         analog_i2c_delay();
     }
-
+    if (!ack)
+        bsp_analog_i2c_nack();//发送nACK
+    else
+        bsp_analog_i2c_ack(); //发送ACK
     return data;
 }

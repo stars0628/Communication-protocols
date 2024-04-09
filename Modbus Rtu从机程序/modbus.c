338d00f7db1b818172dff922854af8bb191aec5d
Stars0628 <43928335+stars0628@users.noreply.github.com>
2024-04-09 16:29:03 +0800

    完善优化modbus协议

--- Modbus Rtu从机程序/modbus.c
+++ Modbus Rtu从机程序/modbus.c
@@ -1,17 +1,15 @@
 
 // modbus-rtu标准的从机程序
 #include "modbus.h"
-uint16_t usDiscreteInputStart;
-uint8_t usDiscreteInputBuf[DISCRETE_INPUT_NDISCRETES / 8];
-uint16_t usCoilStart;
-uint8_t usCoilBuf[COIL_NCOILS / 8];
-uint16_t usRegInputStart;
-uint16_t usRegInputBuf[REG_INPUT_NREGS];
-uint16_t usRegHoldingStart;
-uint16_t usRegHoldingBuf[REG_HOLDING_NREGS];
-__IO uint8_t Tx_Buf[256];  // 发送缓存,最大256字节
-PDUData_TypeDef PduData;
-const static unsigned char auchCRCHi[] = {
+unsigned char MB_SLAVEADDR = 0x0001;//设备地址
+unsigned char usDiscreteInputBuf[DISCRETE_INPUT_NDISCRETES / 8];//离散输入寄存器
+unsigned char usCoilBuf[COIL_NCOILS / 8];//线圈
+unsigned short usRegInputBuf[REG_INPUT_NREGS];//输入寄存器
+unsigned short usRegHoldingBuf[REG_HOLDING_NREGS];//保持寄存器
+__IO unsigned char MB_TxBuf[256];  // 发送缓存,最大256字节
+PDUData_TypeDef PduData;//Modbus数据解析的结构体
+
+const static unsigned char mbCRCHi[] = {
     0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0,
     0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
     0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0,
@@ -38,7 +36,7 @@ const static unsigned char auchCRCHi[] = {
     0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
     0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0,
     0x80, 0x41, 0x00, 0xC1, 0x81, 0x40};
-static const char auchCRCLo[] = {
+static const char mbCRCLo[] = {
     0x00, 0xC0, 0xC1, 0x01, 0xC3, 0x03, 0x02, 0xC2, 0xC6, 0x06,
     0x07, 0xC7, 0x05, 0xC5, 0xC4, 0x04, 0xCC, 0x0C, 0x0D, 0xCD,
     0x0F, 0xCF, 0xCE, 0x0E, 0x0A, 0xCA, 0xCB, 0x0B, 0xC9, 0x09,
@@ -65,12 +63,12 @@ static const char auchCRCLo[] = {
     0x8A, 0x4A, 0x4E, 0x8E, 0x8F, 0x4F, 0x8D, 0x4D, 0x4C, 0x8C,
     0x44, 0x84, 0x85, 0x45, 0x87, 0x47, 0x46, 0x86, 0x82, 0x42,
     0x43, 0x83, 0x41, 0x81, 0x80, 0x40};
-void MB_10H_WR_NReg(uint16_t *_AddrOffset, uint16_t _RegNum, uint8_t *_Datebuf);
-// 把接受到的数据再回复出去
-void MB_SendRX(uint8_t *data, uint32_t length)
-{
-    UART_Transmit((uint8_t *)data, length);
-}
+
+static unsigned short MB_CRC16(unsigned char *puchMsg, unsigned short usDataLen);
+static void MB_Parse_Data(unsigned char *data, unsigned short count);
+static unsigned char MB_Data_Check(unsigned char *data, unsigned short count);
+
+
 /* 函数体 --------------------------------------------------------------------*/
 /**
  * 函数功能: Modbus CRC16 校验计算函数
@@ -78,7 +76,7 @@ void MB_SendRX(uint8_t *data, uint32_t length)
  * 返 回 值: CRC16 计算结果
  * 说    明: 计算结果是高位在前,需要转换才能发送
  */
-uint16_t MB_CRC16(uint8_t *puchMsg, uint16_t usDataLen)
+static unsigned short MB_CRC16(unsigned char *puchMsg, unsigned short usDataLen)
 {
     unsigned char uchCRCHi = 0xFF; /* high byte of CRC initialized */
     unsigned char uchCRCLo = 0xFF; /* low byte of CRC initialized */
@@ -86,36 +84,37 @@ uint16_t MB_CRC16(uint8_t *puchMsg, uint16_t usDataLen)
     while (usDataLen--)            /* pass through message buffer */
     {
         uIndex = uchCRCLo ^ *puchMsg++; /* calculate the CRC */
-        uchCRCLo = uchCRCHi ^ auchCRCHi[uIndex];
-        uchCRCHi = auchCRCLo[uIndex];
+        uchCRCLo = uchCRCHi ^ mbCRCHi[uIndex];
+        uchCRCHi = mbCRCLo[uIndex];
     }
     // 根据需要对校验值处理（高低位不同）
     // return (uchCRCLo << 8 | uchCRCHi) ;//低位在左高位在右
     return (uchCRCHi << 8 | uchCRCLo);  // 高位在左低位在右
 }
+
 /* 提取数据帧,进行解析数据帧 */
-void MB_Parse_Data(uint8_t *data, uint32_t count)
+static void MB_Parse_Data(unsigned char *data, unsigned short count)
 {
+    PduData.SlaveAddr = data[0];
     PduData.Code = data[1];                                            // 功能码
     PduData.Addr = ((data[2] << 8) | data[3]);                         // 寄存器起始地址
     PduData.Num = ((data[4] << 8) | data[5]);                          // 数量(Coil,Input,Holding Reg,Input Reg)
-    PduData._CRC = MB_CRC16((uint8_t *)data, count - 2);               // CRC校验码
+    PduData._CRC = MB_CRC16((unsigned char *)data, count - 2);               // CRC校验码
     PduData.byteNums = data[6];                                        // 获得字节数
-    PduData.ValueReg = (uint8_t *)&data[7];                            // 寄存器值起始地址
+    PduData.ValueReg = (unsigned char *)&data[7];                            // 寄存器值起始地址
     PduData.PtrCoilOffset = PduData.PtrCoilbase + PduData.Addr;        // 离散量的内存起始地址
     PduData.PtrHoldingOffset = PduData.PtrHoldingbase + PduData.Addr;  // 保持寄存器的起始地址
 }
-/**
- * 函数功能: 对接收到的数据进行分析并执行
- * 输入参数: 无
- * 返 回 值: 异常码或0x00
- * 说    明: 判断功能码,验证地址是否正确.数值内容是否溢出,数据没错误就发送响应信号
- */
-uint8_t MB_Analyze_Execute(uint8_t *data, uint32_t count)
+
+/* 校验收到的数据是否合法 */
+static unsigned char MB_Data_Check(unsigned char *data, unsigned short count)
 {
-    uint16_t ExCode = EX_CODE_NONE;
-    uint16_t tem_crc;
-    MB_Parse_Data(data, count);
+    unsigned short ExCode = EX_CODE_NONE;
+    /* 检查地址 */
+    if (PduData.SlaveAddr != MB_SLAVEADDR && PduData.SlaveAddr != MB_ALLSLAVEADDR) {
+        ExCode = EX_CODE_03H;  // 异常码03H
+        return ExCode;
+    }
     /* 校验功能码 */
     if (IS_NOT_FUNCODE(PduData.Code))  // 不支持的功能码
     {
@@ -132,63 +131,83 @@ uint8_t MB_Analyze_Execute(uint8_t *data, uint32_t count)
         ExCode = EX_CODE_04H;
         return ExCode;
     }
+    return ExCode;
+}
+
+/* Modbus功能码1和功能码2 */
+static void MB_Fun1_2(unsigned char* buffer)
+{
+    unsigned short tem_crc;
+    
+    MB_TxBuf[0] = PduData.SlaveAddr;       // 从机地址
+    MB_TxBuf[1] = PduData.Code;  // 功能码
+    if (PduData.Num % 8 == 0)  // 如果读取线圈的数量是8的整数倍，则返回字节数MB_TxBuf[2] = PduData.Num / 8
+        MB_TxBuf[2] = PduData.Num / 8;
+    else  // 如果不是8的整数倍，则加一
+        MB_TxBuf[2] = PduData.Num / 8 + 1;
+    for (int i = 0; i < MB_TxBuf[2]; i++) {
+        MB_TxBuf[3 + i] = (buffer[PduData.Addr + 7 + i * 8] << 7) | 
+                        (buffer[PduData.Addr + 6 + i * 8] << 6) | 
+                        (buffer[PduData.Addr + 5 + i * 8] << 5) | 
+                        (buffer[PduData.Addr + 4 + i * 8] << 4) | 
+                        (buffer[PduData.Addr + 3 + i * 8] << 3) | 
+                        (buffer[PduData.Addr + 2 + i * 8] << 2) | 
+                        (buffer[PduData.Addr + 1 + i * 8] << 1) | 
+                        (buffer[PduData.Addr + 0 + i * 8] << 0);
+    }
+    tem_crc = MB_CRC16((unsigned char *)MB_TxBuf, MB_TxBuf[2] + 3);  // CRC校验码
+    MB_TxBuf[MB_TxBuf[2] + 3] = (unsigned char)tem_crc;
+    MB_TxBuf[MB_TxBuf[2] + 4] = (unsigned char)(tem_crc >> 8);
+    MB_UartSend((unsigned char *)MB_TxBuf, MB_TxBuf[2] + 5);
+}
+
+/* Modbus功能码3和功能码4 */
+static void MB_Fun3_4(unsigned short *buffer)
+{
+    unsigned short tem_crc;
+    MB_TxBuf[0] = PduData.SlaveAddr;
+    MB_TxBuf[1] = PduData.Code;
+    MB_TxBuf[2] = PduData.Num * 2;
+    for (unsigned char i = 0; i < PduData.Num; i++) {
+        MB_TxBuf[i * 2 + 3] = (unsigned char)(buffer[PduData.Addr + i] >> 8);
+        MB_TxBuf[i * 2 + 4] = (unsigned char)(buffer[PduData.Addr + i] >> 0);
+    }
+    tem_crc = MB_CRC16((unsigned char *)MB_TxBuf, PduData.Num * 2 + 3);  // CRC校验码
+    MB_TxBuf[PduData.Num * 2 + 3] = (unsigned char)tem_crc;
+    MB_TxBuf[PduData.Num * 2 + 4] = (unsigned char)(tem_crc >> 8);
+    MB_UartSend((unsigned char *)MB_TxBuf, PduData.Num * 2 + 5);
+}
+
+
+/**
+ * 函数功能: 对接收到的数据进行分析并执行
+ * 输入参数: 无
+ * 返 回 值: 异常码或0x00
+ * 说    明: 判断功能码,验证地址是否正确.数值内容是否溢出,数据没错误就发送响应信号
+ */
+unsigned char MB_Analyze_Execute(unsigned char *data, unsigned short count)
+{
+    unsigned short ExCode = EX_CODE_NONE;
+    unsigned short tem_crc;
+    MB_Parse_Data(data, count);//解析数据帧
+
+    if(MB_Data_Check(data,count) != EX_CODE_NONE) { //判断数据是否合法
+        return ExCode;
+    }
+    
     /* 根据功能码分别做判断 */
     switch (PduData.Code) {
     case FUN_CODE_01H:             // 读线圈寄存器
-        Tx_Buf[0] = data[0];       // 从机地址
-        Tx_Buf[1] = PduData.Code;  // 功能码
-        if (PduData.Num % 8 == 0)  // 如果读取线圈的数量是8的整数倍，则返回字节数Tx_Buf[2] = PduData.Num / 8
-            Tx_Buf[2] = PduData.Num / 8;
-        else  // 如果不是8的整数倍，则加一
-            Tx_Buf[2] = PduData.Num / 8 + 1;
-        for (int i = 0; i < Tx_Buf[2]; i++) {
-            Tx_Buf[3 + i] = (usCoilBuf[PduData.Addr + 7 + i * 8] << 7) | (usCoilBuf[PduData.Addr + 6 + i * 8] << 6) | (usCoilBuf[PduData.Addr + 5 + i * 8] << 5) | (usCoilBuf[PduData.Addr + 4 + i * 8] << 4) | (usCoilBuf[PduData.Addr + 3 + i * 8] << 3) | (usCoilBuf[PduData.Addr + 2 + i * 8] << 2) | (usCoilBuf[PduData.Addr + 1 + i * 8] << 1) | (usCoilBuf[PduData.Addr + 0 + i * 8] << 0);
-        }
-        tem_crc = MB_CRC16((uint8_t *)Tx_Buf, Tx_Buf[2] + 3);  // CRC校验码
-        Tx_Buf[Tx_Buf[2] + 3] = (uint8_t)tem_crc;
-        Tx_Buf[Tx_Buf[2] + 4] = (uint8_t)(tem_crc >> 8);
-        UART_Transmit((uint8_t *)Tx_Buf, Tx_Buf[2] + 5);
+        MB_Fun1_2(usCoilBuf);
         break;
-    case FUN_CODE_02H:             // 读离散输入寄存器，其实读离散输入寄存器和读线圈寄存器类似
-        Tx_Buf[0] = data[0];       // 从机地址
-        Tx_Buf[1] = PduData.Code;  // 功能码
-        if (PduData.Num % 8 == 0)  // 如果读取线圈的数量是8的整数倍，则返回字节数Tx_Buf[2] = PduData.Num / 8
-            Tx_Buf[2] = PduData.Num / 8;
-        else  // 如果不是8的整数倍，则加一
-            Tx_Buf[2] = PduData.Num / 8 + 1;
-        for (int i = 0; i < Tx_Buf[2]; i++) {
-            Tx_Buf[3 + i] = (usDiscreteInputBuf[PduData.Addr + 7 + i * 8] << 7) | (usDiscreteInputBuf[PduData.Addr + 6 + i * 8] << 6) | (usDiscreteInputBuf[PduData.Addr + 5 + i * 8] << 5) | (usDiscreteInputBuf[PduData.Addr + 4 + i * 8] << 4) | (usDiscreteInputBuf[PduData.Addr + 3 + i * 8] << 3) | (usDiscreteInputBuf[PduData.Addr + 2 + i * 8] << 2) | (usDiscreteInputBuf[PduData.Addr + 1 + i * 8] << 1) | (usDiscreteInputBuf[PduData.Addr + 0 + i * 8] << 0);
-        }
-        tem_crc = MB_CRC16((uint8_t *)Tx_Buf, Tx_Buf[2] + 3);  // CRC校验码
-        Tx_Buf[Tx_Buf[2] + 3] = (uint8_t)tem_crc;
-        Tx_Buf[Tx_Buf[2] + 4] = (uint8_t)(tem_crc >> 8);
-        UART_Transmit((uint8_t *)Tx_Buf, Tx_Buf[2] + 5);
+    case FUN_CODE_02H:             // 读离散输入寄存器，其实读离散输入寄存器和读线圈寄存器类似，不过离散输入寄存器是只读的
+        MB_Fun1_2(usDiscreteInputBuf);
         break;
     case FUN_CODE_03H:  // 响应读保持寄存器
-        Tx_Buf[0] = data[0];
-        Tx_Buf[1] = PduData.Code;
-        Tx_Buf[2] = PduData.Num * 2;
-        for (uint8_t i = 0; i < PduData.Num; i++) {
-            Tx_Buf[i * 2 + 3] = (uint8_t)(usRegInputBuf[PduData.Addr + i] >> 8);
-            Tx_Buf[i * 2 + 4] = (uint8_t)(usRegInputBuf[PduData.Addr + i] >> 0);
-        }
-        tem_crc = MB_CRC16((uint8_t *)Tx_Buf, PduData.Num * 2 + 3);  // CRC校验码
-        Tx_Buf[PduData.Num * 2 + 3] = (uint8_t)tem_crc;
-        Tx_Buf[PduData.Num * 2 + 4] = (uint8_t)(tem_crc >> 8);
-        UART_Transmit((uint8_t *)Tx_Buf, PduData.Num * 2 + 5);
+        MB_Fun3_4(usRegHoldingBuf);
         break;
     case FUN_CODE_04H:  // 响应读输入寄存器
-        Tx_Buf[0] = data[0];
-        Tx_Buf[1] = PduData.Code;
-        Tx_Buf[2] = PduData.Num * 2;
-        for (uint8_t i = 0; i < PduData.Num; i++) {
-            Tx_Buf[i * 2 + 3] = (uint8_t)(usRegInputBuf[PduData.Addr + i] >> 8);
-            Tx_Buf[i * 2 + 4] = (uint8_t)(usRegInputBuf[PduData.Addr + i] >> 0);
-        }
-        tem_crc = MB_CRC16((uint8_t *)Tx_Buf, PduData.Num * 2 + 3);  // CRC校验码
-        Tx_Buf[PduData.Num * 2 + 3] = (uint8_t)tem_crc;
-        Tx_Buf[PduData.Num * 2 + 4] = (uint8_t)(tem_crc >> 8);
-        UART_Transmit((uint8_t *)Tx_Buf, PduData.Num * 2 + 5);
+        MB_Fun3_4(usRegInputBuf);
         break;
     case FUN_CODE_05H:
         /* 写入一个线圈值 */
@@ -197,60 +216,42 @@ uint8_t MB_Analyze_Execute(uint8_t *data, uint32_t count)
         } else {
             usCoilBuf[PduData.Addr] = 0;
         }
-        MB_SendRX(data, count);  // 返回发送的指令作为响应
+        MB_UartSend(data, count);  // 返回发送的指令作为响应
         break;
     case FUN_CODE_06H:  // 写单个保持寄存器
         usRegHoldingBuf[PduData.Addr] = (data[4] << 8) | (data[5] << 0);
-        MB_SendRX(data, count);  // 返回发送的指令作为响应
+        MB_UartSend(data, count);  // 返回发送的指令作为响应
         break;
     case FUN_CODE_10H:  // 写入多个保持寄存器
         for (int i = 0; i < PduData.Num; i++) {
             usRegHoldingBuf[PduData.Addr + i] = (data[i * 2 + 6] << 8) | (data[i * 2 + 7] << 0);
         }
         // 响应写多个保持寄存器
-        Tx_Buf[0] = data[0];  // 从机地址
-        Tx_Buf[1] = data[1];  // 功能码
-        Tx_Buf[2] = data[2];  // 起始地址高位
-        Tx_Buf[3] = data[3];  // 起始地址低位
-        Tx_Buf[4] = data[4];  // 数量高位
-        Tx_Buf[5] = data[5];  // 数量低位
-        tem_crc = MB_CRC16((uint8_t *)Tx_Buf, 6);  // CRC校验码
-        Tx_Buf[6] = (uint8_t)tem_crc;              // CRC高位
-        Tx_Buf[7] = (uint8_t)(tem_crc >> 8);       // CRC低位
-        UART_Transmit((uint8_t *)Tx_Buf, 8);
+        MB_TxBuf[0] = PduData.SlaveAddr;  // 从机地址
+        MB_TxBuf[1] = data[1];  // 功能码
+        MB_TxBuf[2] = data[2];  // 起始地址高位
+        MB_TxBuf[3] = data[3];  // 起始地址低位
+        MB_TxBuf[4] = data[4];  // 数量高位
+        MB_TxBuf[5] = data[5];  // 数量低位
+        tem_crc = MB_CRC16((unsigned char *)MB_TxBuf, 6);  // CRC校验码
+        MB_TxBuf[6] = (unsigned char)tem_crc;              // CRC高位
+        MB_TxBuf[7] = (unsigned char)(tem_crc >> 8);       // CRC低位
+        MB_UartSend((unsigned char *)MB_TxBuf, 8);
         break;
     }
     /* 数据帧没有异常 */
     return ExCode;  //   EX_CODE_NONE
 }
-/**
- * 函数功能: 写,读N个寄存器(不知道是啥，先保留)
- * 输入参数: _AddrOffset:偏移地址,_RegNum:寄存器数量,_Datebuf:数据指针
- * 返 回 值: 异常码:04H或NONE
- * 说    明: 在_AddrOffset所指向的空间里写入_RegNum*2个数据,并且读取验证是否写入成功
- */
-void MB_10H_WR_NReg(uint16_t *_AddrOffset, uint16_t _RegNum, uint8_t *_Datebuf)
-{
-    uint16_t i = 0;
-    uint16_t Value = 0;
-    for (i = 0; i < _RegNum; i++) {
-        Value = (uint16_t)((*_Datebuf << 8) | (*(_Datebuf + 1)));
-        *_AddrOffset++ = Value;
-        _Datebuf += 2;
-    }
-    // MB_SendRX(,);
-}
+
 /**
  * 函数功能: 串口发送函数
  * 输入参数: data:要发送的buff,length:要发送的长度
  * 返 回 值:
  * 说    明: 发送指定长度的串口数据
  */
-void UART_Transmit(uint8_t *data, uint32_t length)
+void MB_UartSend(unsigned char *data, unsigned short length)
 {
     // 在此处将data的数据传入串口发送buff，并通过串口发送出去
-    for (int i = 0; i < length; i++) {
-        U1_TxBuffer[i] = data[i];
-    }
-    DMA_send(length);
+    HAL_UART_Transmit_DMA(&huart1,data,length);
 }
+

9915f568513b268124c6b3fb251e539ecba0ea3c
Stars0628 <cxc1999628@foxmail.com>
2024-08-14 19:27:31 +0800

    修改了软件SPI的bug，完善了模拟SPI函数

--- 软件SPI/soft_spi.c
+++ 软件SPI/soft_spi.c
@@ -1,255 +1,218 @@
 #include "soft_spi.h"
 
+/**************************************************************************************
+ * 描  述 : 模拟SPI写入一个数据
+ * 入  参 : TxData：待发送的数据地址
+ * 返回值 : 无
+ **************************************************************************************/
+void SPI_Write(const unsigned char *TxData) {
+  unsigned char i;
+  unsigned short *tx_ptr;
+  unsigned short tx_temp;
+  tx_ptr = (unsigned short *)TxData;
+  tx_temp = *tx_ptr;
 #if SPI_MODE == 0
-/**************************************************************************************
- * 描  述 : 模拟SPI写入一个数据
- * 入  参 : unsigned char date
- * 返回值 : 无
- **************************************************************************************/
-void SPI_Write(unsigned char TxData) {
-  unsigned char temp = TxData, i;
   for (i = 0; i < SPI_DATA_SIZE; i++) {
     SPI_SCK_L;
-    if (temp & SPI_MSB_MASK) {
+    SPI_Delay;
+    if (tx_temp & MSB_OR_LSB_MASK) {
       SPI_MOSI_H;
     } else {
       SPI_MOSI_L;
     }
+    SPI_Delay;
     SPI_SCK_H;
-    temp <<= 1;
+    SPI_Delay;
+    TX_TEMP_MOVE;
   }
   SPI_SCK_L;
-}
-
-/**************************************************************************************
- * 描  述 : 模拟SPI读取一个数据
- * 入  参 : 无
- * 返回值 : 读取unsigned char数据
- **************************************************************************************/
-unsigned char SPI_Read(void) {
-  unsigned char i, temp = 0;
-  for (i = 0; i < SPI_DATA_SIZE; i++) {
-    temp <<= 1;
-    SPI_SCK_L;
-    if (MISO_READ) {
-      temp++;
-    }
-    SPI_SCK_H;
-  }
-  SPI_SCK_L;
-  return temp;
-}
-
-/**************************************************************************************
- * 描  述 : 模拟SPI读写一个数据
- * 入  参 : unsigned char TxData, unsigned char *RdData
- * 返回值 : 无
- **************************************************************************************/
-void SPI_WriteRead(unsigned char TxData, unsigned char *RxData) {
-  unsigned char i, temp = 0;
-  for (i = 0; i < SPI_DATA_SIZE; i++) {
-    SPI_SCK_L;
-    if (TxData & SPI_MSB_MASK) {
-      SPI_MOSI_H;
-    } else {
-      SPI_MOSI_L;
-    }
-    TxData <<= 1;
-    SPI_SCK_H;
-    temp <<= 1;
-    if (MISO_READ) {
-      temp |= 0x01;
-    }
-  }
-  SPI_SCK_L;
-  *RxData = temp;
-}
-
 #elif SPI_MODE == 1
-/**************************************************************************************
- * 描  述 : 模拟SPI写入一个数据
- * 入  参 : unsigned char date
- * 返回值 : 无
- **************************************************************************************/
-void SPI_Write(unsigned char TxData) {
-  unsigned char temp = TxData, i;
   for (i = 0; i < SPI_DATA_SIZE; i++) {
     SPI_SCK_H;
-    if (temp & SPI_MSB_MASK) {
+    SPI_Delay;
+    if (tx_temp & MSB_OR_LSB_MASK) {
       SPI_MOSI_H;
     } else {
       SPI_MOSI_L;
     }
+    SPI_Delay;
     SPI_SCK_L;
-    temp <<= 1;
+    SPI_Delay;
+    TX_TEMP_MOVE;
   }
-}
-
-/**************************************************************************************
- * 描  述 : 模拟SPI读取一个数据
- * 入  参 : 无
- * 返回值 : 读取unsigned char数据
- **************************************************************************************/
-unsigned char SPI_Read(void) {
-  unsigned char i, temp = 0;
-  for (i = 0; i < SPI_DATA_SIZE; i++) {
-    temp <<= 1;
-    SPI_SCK_H;
-    SPI_SCK_L;
-    if (MISO_READ) {
-      temp++;
-    }
-  }
-  return temp;
-}
-
-/**************************************************************************************
- * 描  述 : 模拟SPI读写一个数据
- * 入  参 : unsigned char TxData, unsigned char *RdData
- * 返回值 : 无
- **************************************************************************************/
-void SPI_WriteRead(unsigned char TxData, unsigned char *RxData) {
-  unsigned char i, temp = 0;
-  for (i = 0; i < SPI_DATA_SIZE; i++) {
-    SPI_SCK_H;
-    if (TxData & SPI_MSB_MASK) {
-      SPI_MOSI_H;
-    } else {
-      SPI_MOSI_L;
-    }
-    TxData <<= 1;
-    SPI_SCK_L;
-    temp <<= 1;
-    if (MISO_READ) {
-      temp |= 0x01;
-    }
-  }
-  *RxData = temp;
-}
-
 #elif SPI_MODE == 2
-/**************************************************************************************
- * 描  述 : 模拟SPI写入一个数据
- * 入  参 : unsigned char date
- * 返回值 : 无
- **************************************************************************************/
-void SPI_Write(unsigned char TxData) {
-  unsigned char temp = TxData, i;
   for (i = 0; i < SPI_DATA_SIZE; i++) {
     SPI_SCK_H;
-    if (temp & SPI_MSB_MASK) {
+    SPI_Delay;
+    if (tx_temp & MSB_OR_LSB_MASK) {
       SPI_MOSI_H;
     } else {
       SPI_MOSI_L;
     }
+    SPI_Delay;
     SPI_SCK_L;
-    temp <<= 1;
+    SPI_Delay;
+    TX_TEMP_MOVE;
   }
   SPI_SCK_H;
-}
-
-/**************************************************************************************
- * 描  述 : 模拟SPI读取一个数据
- * 入  参 : 无
- * 返回值 : 读取unsigned char数据
- **************************************************************************************/
-unsigned char SPI_Read(void) {
-  unsigned char i, temp = 0;
-  for (i = 0; i < SPI_DATA_SIZE; i++) {
-    temp <<= 1;
-    SPI_SCK_H;
-    SPI_SCK_L;
-    if (MISO_READ) {
-      temp++;
-    }
-  }
-  SPI_SCK_H;
-  return temp;
-}
-
-/**************************************************************************************
- * 描  述 : 模拟SPI读写一个数据
- * 入  参 : unsigned char TxData, unsigned char *RdData
- * 返回值 : 无
- **************************************************************************************/
-void SPI_WriteRead(unsigned char TxData, unsigned char *RxData) {
-  unsigned char i, temp = 0;
-  for (i = 0; i < SPI_DATA_SIZE; i++) {
-    SPI_SCK_H;
-    if (TxData & SPI_MSB_MASK) {
-      SPI_MOSI_H;
-    } else {
-      SPI_MOSI_L;
-    }
-    TxData <<= 1;
-    SPI_SCK_L;
-    temp <<= 1;
-    if (MISO_READ) {
-      temp |= 0x01;
-    }
-  }
-  SPI_SCK_H;
-  *RxData = temp;
-}
 #elif SPI_MODE == 3
-/**************************************************************************************
- * 描  述 : 模拟SPI写入一个数据
- * 入  参 : unsigned char date
- * 返回值 : 无
- **************************************************************************************/
-void SPI_Write(unsigned char TxData) {
-  unsigned char temp = TxData, i;
   for (i = 0; i < SPI_DATA_SIZE; i++) {
     SPI_SCK_L;
-    if (temp & SPI_MSB_MASK) {
+    SPI_Delay;
+    if (tx_temp & MSB_OR_LSB_MASK) {
       SPI_MOSI_H;
     } else {
       SPI_MOSI_L;
     }
+    SPI_Delay;
     SPI_SCK_H;
-    temp <<= 1;
+    TX_TEMP_MOVE;
   }
+#endif
 }
 
 /**************************************************************************************
  * 描  述 : 模拟SPI读取一个数据
- * 入  参 : 无
- * 返回值 : 读取unsigned char数据
+ * 入  参 : RxData：接收数据的地址
+ * 返回值 : 无
  **************************************************************************************/
-unsigned char SPI_Read(void) {
-  unsigned char i, temp = 0;
+void SPI_Read(unsigned char *RxData) {
+  unsigned char i;
+  unsigned short rx_temp;
+#if SPI_MODE == 0
   for (i = 0; i < SPI_DATA_SIZE; i++) {
-    temp <<= 1;
+    RX_TEMP_MOVE;
     SPI_SCK_L;
+    SPI_Delay;
     SPI_SCK_H;
+    SPI_Delay;
     if (MISO_READ) {
-      temp++;
+      RX_TEMP_GET
     }
   }
-  return temp;
+  SPI_SCK_L;
+#elif SPI_MODE == 1
+  for (i = 0; i < SPI_DATA_SIZE; i++) {
+    RX_TEMP_MOVE;
+    SPI_SCK_H;
+    SPI_Delay;
+    SPI_SCK_L;
+    SPI_Delay;
+    if (MISO_READ) {
+      RX_TEMP_GET
+    }
+  }
+#elif SPI_MODE == 2
+  for (i = 0; i < SPI_DATA_SIZE; i++) {
+    RX_TEMP_MOVE;
+    SPI_SCK_H;
+    SPI_Delay;
+    SPI_SCK_L;
+    SPI_Delay;
+    if (MISO_READ) {
+      RX_TEMP_GET
+    }
+  }
+  SPI_SCK_H;
+#elif SPI_MODE == 3
+  for (i = 0; i < SPI_DATA_SIZE; i++) {
+    RX_TEMP_MOVE;
+    SPI_SCK_L;
+    SPI_Delay;
+    SPI_SCK_H;
+    SPI_Delay;
+    if (MISO_READ) {
+      RX_TEMP_GET
+    }
+  }
+#endif
+  *(uint16_t *)RxData = rx_temp;
 }
 
 /**************************************************************************************
  * 描  述 : 模拟SPI读写一个数据
- * 入  参 : unsigned char TxData, unsigned char *RdData
+ * 入  参 : TxData：待发送的数据地址
+            RxData：接收数据的地址
  * 返回值 : 无
  **************************************************************************************/
-void SPI_WriteRead(unsigned char TxData, unsigned char *RxData) {
-  unsigned char i, temp = 0;
+void SPI_WriteRead(unsigned char *TxData, unsigned char *RxData) {
+  unsigned char i;
+  unsigned short *tx_ptr;
+  unsigned short tx_temp, rx_temp;
+  tx_ptr = (unsigned short *)TxData;
+  tx_temp = *tx_ptr;
+#if SPI_MODE == 0
   for (i = 0; i < SPI_DATA_SIZE; i++) {
     SPI_SCK_L;
-    if (TxData & SPI_MSB_MASK) {
+    SPI_Delay;
+    if (tx_temp & MSB_OR_LSB_MASK) {
       SPI_MOSI_H;
     } else {
       SPI_MOSI_L;
     }
-    TxData <<= 1;
+    SPI_Delay TX_TEMP_MOVE;
     SPI_SCK_H;
-    temp <<= 1;
+    SPI_Delay;
+    RX_TEMP_MOVE;
     if (MISO_READ) {
-      temp |= 0x01;
+      RX_TEMP_GET
     }
   }
-  *RxData = temp;
-}
+  SPI_SCK_L;
+#elif SPI_MODE == 1
+  for (i = 0; i < SPI_DATA_SIZE; i++) {
+    if (tx_temp & MSB_OR_LSB_MASK) {
+      SPI_MOSI_H;
+    } else {
+      SPI_MOSI_L;
+    }
+    SPI_Delay TX_TEMP_MOVE;
+    SPI_SCK_H;
+    SPI_Delay;
+    SPI_SCK_L;
+    SPI_Delay;
+    RX_TEMP_MOVE;
+    if (MISO_READ) {
+      RX_TEMP_GET
+    }
+  }
+#elif SPI_MODE == 2
+  for (i = 0; i < SPI_DATA_SIZE; i++) {
+    SPI_SCK_H;
+    SPI_Delay;
+    if (tx_temp & MSB_OR_LSB_MASK) {
+      SPI_MOSI_H;
+    } else {
+      SPI_MOSI_L;
+    }
+    SPI_Delay;
+    TX_TEMP_MOVE;
+    SPI_SCK_L;
+    SPI_Delay;
+    RX_TEMP_MOVE;
+    if (MISO_READ) {
+      RX_TEMP_GET
+    }
+  }
+  SPI_SCK_H;
+#elif SPI_MODE == 3
+  for (i = 0; i < SPI_DATA_SIZE; i++) {
+    SPI_SCK_L;
+    SPI_Delay;
+    if (tx_temp & MSB_OR_LSB_MASK) {
+      SPI_MOSI_H;
+    } else {
+      SPI_MOSI_L;
+    }
+    SPI_Delay;
+    TX_TEMP_MOVE;
+    SPI_SCK_H;
+    SPI_Delay;
+    RX_TEMP_MOVE;
+    if (MISO_READ) {
+      RX_TEMP_GET
+    }
+  }
 #endif
+  *(uint16_t *)RxData = rx_temp;
+}
--- 软件SPI/soft_spi.h
+++ 软件SPI/soft_spi.h
@@ -15,12 +15,42 @@
 
 // SPI数据数据帧位数
 #define SPI_DATA_SIZE 8
-// SPI工作模式（允许值0、1、2、3）
-#define SPI_MODE 3
-// SPI数据高位判断条件
+
+// SPI高低位优先（0:MSB优先、other:LSB优先）
+#define MSB_LSB 0
+
+// SPI模式（0、1、2、3）
+#define SPI_MODE 0
+
+// 引脚电平变换后延时
+#define SPI_Delay 
+
+// MSB、LSB掩码
 #define SPI_MSB_MASK (1 << (SPI_DATA_SIZE - 1))
+#define SPI_LSB_MASK (1)
+
+#if MSB_LSB == 0
+// 发送过程中间值的操作
+#define TX_TEMP_MOVE tx_temp <<= 1 
+#define RX_TEMP_MOVE rx_temp <<= 1
+// 判断高位为1
+#define MSB_OR_LSB_MASK SPI_MSB_MASK
+// 对读取数据的操作
+#define RX_TEMP_GET rx_temp |= SPI_LSB_MASK;
+#else
+// 发送过程中间值的操作
+#define TX_TEMP_MOVE tx_temp >>= 1
+#define RX_TEMP_MOVE rx_temp >>= 1
+// 判断低位为1
+#define MSB_OR_LSB_MASK SPI_LSB_MASK
+// 对读取数据的操作
+#define RX_TEMP_GET rx_temp |= SPI_MSB_MASK;
+#endif
+
 
 // SPI函数
-void SPI_Write(unsigned char TxData);
-unsigned char SPI_Read(void);
-void SPI_WriteRead(unsigned char TxData, unsigned char *RxData);
+void SPI_Write(const unsigned char *TxData);
+void SPI_Read(unsigned char *RxData);
+void SPI_WriteRead(unsigned char *TxData, unsigned char *RxData);
+
+#endif
